bc6e4c47228a8b2a453310171568c985e81e4e97
PhucLeDio <dinhphucle3@gmail.com>
2024-09-15 22:16:29 +0700

    feat: Upload file setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,13 @@
-import type { Config } from "tailwindcss";
+// import type { Config } from "tailwindcss";
+import { withUt } from "uploadthing/tw";
 
-const config: Config = {
-    darkMode: ["class"],
+export default withUt({
+	darkMode: ["class"],
     content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  	],
   theme: {
   	extend: {
   		colors: {
@@ -59,5 +60,4 @@ const config: Config = {
   	}
   },
   plugins: [require("tailwindcss-animate")],
-};
-export default config;
+});
