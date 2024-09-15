35eafafe27aaac135ae98060543baf8416c17425
PhucLeDio <dinhphucle3@gmail.com>
2024-09-15 16:42:53 +0700

    feat: initial modal UI

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,6 +1,7 @@
 import { redirect } from "next/navigation";
 import { initialProfile } from "@/lib/initial-profile";
 import { db } from "@/lib/db";
+import { InitialModal } from "@/components/modals/initial-modal";
 
 const SetupPage = async () => {
     const profile = await initialProfile();
@@ -24,7 +25,7 @@ const SetupPage = async () => {
     }
 
     return ( 
-        <div>Create a server</div>
+        <InitialModal />
     );
 }
  
