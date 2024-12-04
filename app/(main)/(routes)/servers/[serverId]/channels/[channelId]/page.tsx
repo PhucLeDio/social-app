210d9fad86d30a5ec8a5a2f3f0820b27c30a80f1
chouthi <136318153+chouthi@users.noreply.github.com>
2024-12-04 16:01:28 +0700

    Emoji bar

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,4 +1,5 @@
 import { ChatHeader } from "@/components/chat/chat-header";
+import { ChatInput } from "@/components/chat/chat-input";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { RedirectToSignIn } from "@clerk/nextjs";
@@ -46,6 +47,19 @@ const ChannelIdPage = async ({
             serverId={channel.serverId}
             name={channel.name} 
             type="channel" 
+            />
+            <div className="flex-1">
+                Future Mess
+            </div>
+            <ChatInput 
+            name={channel.name}
+            type="channel"
+            apiUrl={"/api/socket/messages"} 
+            query={{
+                channelId: channel.id,
+                serverId: channel.serverId,
+            }} 
+    
             />
         </div>
     );
